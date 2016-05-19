15d0f36d925cbf999ed94da77b3ccf8409c132af
Chris Metcalfe <metcalfe.c@gmail.com>
2016-05-18 22:37:31 -0700

    FindItem functionality is working. Displays item results properly. #1

--- public/default.js
+++ public/default.js
@@ -7,22 +7,34 @@ var productsTemp = [
   }
 ]
 
-$("#search-btn").click(function(event) {
-  event.preventDefault();
-  if (findItem($("#search-txt").val())) {
-    console.log(true);
-  } else {
-    console.log(false);
-  }
+$(document).ready(function() {
+  $("#search-btn").click(function(event) {
+    event.preventDefault();
+    if (findItem($("#search-txt").val())) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  });
 });
 
 function findItem(item) {
   for (var i = 0; i < productsTemp.length; i++) {
     for (var j = 0; j < productsTemp[i].keywords.length; j++) {
-      if (productsTemp[i].keywords[j] === item) {
-        return true;
+      if (productsTemp[i].keywords[j] === item.toLowerCase()) {
+        var productCol = $('<div class="col-md-3"></div>');
+        var productThumb = $('<div class="thumbnail"></div>');
+        var productImg = $('<img src="' + productsTemp[i].image + '">')
+        var productTitleDiv = $('<div class="product-title">')
+        var productTitle = $('<a href="#"><h5>' + productsTemp[i].name + '</h5></a>')
+        var productPrice = $('<p class="product-price">$' + productsTemp[i].price + '</p>')
+        $('#results').append(productCol);
+        $(productCol).append(productThumb);
+        $(productThumb).append(productImg);
+        $(productThumb).append(productTitleDiv);
+        $(productTitleDiv).append(productTitle);
+        $(productTitleDiv).append(productPrice);
       }
     }
   }
-  return false;
 }
